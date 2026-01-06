3ef827020a23a01532ea330cfac476f26516a5df
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2026-01-06 18:29:27 -0500

    wip: added blueberry dark colorscheme

--- lua/blueberry/init.lua
+++ lua/blueberry/init.lua
@@ -2,6 +2,18 @@ local M = {}
 
 function M.setup()
 
+  local colors = {
+    bg = "#89b4fa",
+    fg = "#b4befe",
+    red = "#df4576",
+    green = "#00ffd2",
+    blue = "#004687",
+    yellow = "#f9e2af",
+    purple = "#cba6f7",
+    cyan = "#89dceb",
+    gray = "#9399b2",
+  }
+
 end
 
 return M
